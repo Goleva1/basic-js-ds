d08b7325871854f0980c9b6d653ffa9ed97ff7e5
github-Goleva1 <golevaviktoria1@gmai.com>
2025-01-03 11:55:52 +0100

    feat:add solution binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,94 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.rootNode = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const node = new Node(data);
+    this.rootNode === null ? this.rootNode = node : this.addNodeToTree(this.rootNode, node);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addNodeToTree(whereAdd, whatAdd) {
+    if (whereAdd.data > whatAdd.data) {
+      whereAdd.left === null ? whereAdd.left = whatAdd : this.addNodeToTree(whereAdd.left, whatAdd);
+    } else {
+      whereAdd.right === null ? whereAdd.right = whatAdd : this.addNodeToTree(whereAdd.right, whatAdd);
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return this.lookForData(this.rootNode, data) !== null;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  lookForData(where, what) {
+    if (where === null) return null;
+    if (where.data === what) return where;
+    else if (where.data > what) return this.lookForData(where.left, what);
+    else return this.lookForData(where.right, what);
+  }
+
+  find(data) {
+    return this.lookForData(this.rootNode, data);
+  }
+
+  remove(data) {
+    this.rootNode = this.deleteNode(this.rootNode, data);
+  }
+
+  deleteNode(where, what) {
+    if (where === null) return null;
+    if (where.data === what) {
+      if (where.left === null && where.right === null) {
+        where = null;
+        return where;
+      } else if (where.left === null) {
+        return where.right;
+      } else if (where.right === null) {
+        return where.left;
+      } else {
+        const tmp = this.findMin(where.right);
+        where.data = tmp.data;
+        where.right = this.deleteNode(where.right, tmp.data);
+        return where;
+      }
+    } else if (what < where.data) {
+      where.left = this.deleteNode(where.left, what);
+      return where;
+    }
+    else {
+      where.right = this.deleteNode(where.right, what);
+      return where;
+    }
+  }
+
+  findMin(where) {
+    if (where.left === null) return where;
+    return this.findMin(where.left);
+  }
+
+  findMax(where) {
+    if (where.right === null) return where;
+    return this.findMax(where.right);
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.findMin(this.rootNode).data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.findMax(this.rootNode).data;
   }
 }
 
